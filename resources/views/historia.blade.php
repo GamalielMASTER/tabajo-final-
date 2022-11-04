5447354dc59721e54e80dd38a0ae53c993e79e00
GamalielMASTER <dual.leon.solis@gmail.com>
2022-11-04 17:30:55 -0500

    tersera parte

--- resources/views/historia.blade.php
+++ resources/views/historia.blade.php
@@ -8,32 +8,46 @@
     <!-- CSS only -->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
 </head>
+
 <body>
+    
     <header>
     <nav class="navbar navbar-light" style="background-color:red;">
   <a class="navbar-brand">LA PAGINA DE GAMALIEL</a>
   <form class="form-inline">
     <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">buscar</button>
+    <button type="button" class="btn btn-primary btn-lg">Large button</button>
   </form>
     </nav>
 </header>
+<h1> 
+<div class="dropdown">
+  <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+    Dropdown button
+  </button>
+  <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+    <a class="dropdown-item" href="#">Action</a>
+    <a class="dropdown-item" href="#">Another action</a>
+    <a class="dropdown-item" href="#">Something else here</a>
+  </div>
+</div>
+</h1>
 <article>
 <form>
-  <div class="form-group">
-    <label for="exampleInputEmail1">Email address</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+  <div class="alert alert-primary" role="alert">
+    <label for="exampleInputEmail1">correo electronico</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="ingrese su correo">
+    <small id="emailHelp" class="form-text text-muted">Nunca compartiremos tu correo electrónico con nadie más.</small>
   </div>
-  <div class="form-group">
-    <label for="exampleInputPassword1">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+  <div class="alert alert-primary" role="alert">
+    <label for="exampleInputPassword1">contraseña</label>
+    <input type="contraseña" class="form-control" id="exampleInputPassword1" placeholder="ingrese su contraseña">
   </div>
-  <div class="form-check">
+  <div class="alert alert-danger" role="alert">
     <input type="checkbox" class="form-check-input" id="exampleCheck1">
-    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+    <label class="form-check-label" for="exampleCheck1">mostrar la contraseña</label>
   </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
+  <button type="submit" class="btn btn-primary">INICIAR SECCION</button>
 </form>
 
 
